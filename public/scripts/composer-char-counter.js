eebcfe43036159bf7b16de17f309eb82b19b4047
Brandon Lecky <dreamxinxcode@gmail.com>
2020-08-14 17:37:22 +0000

    implemented character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('#tweet-text').on('keyup', function() {
+    const $textLength = $(this).val().length;
+    $('.counter').html(140 - $textLength);
+    if (Number($('.counter').html()) <= 0) {
+      $('.counter').addClass('counter-red');
+    } else {
+      $('.counter').removeClass('counter-red');
+    }
+  });
+});
